ad314fbd0c0695a8727a6ba08edf84f86ee2bb7a
Sean Li <lishang106@gmail.com>
2014-05-14 22:40:00 -0400

    Console log.

--- web/js/render.js
+++ web/js/render.js
@@ -1,5 +1,6 @@
 var showPlaceholder = function (page, status) {
 
+  console.log('TEST: ' + page);
   console.log('TEST: ' + page.data('page'));
 
   var placeholder = page.querySelector('.placeholder');
