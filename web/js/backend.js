1be29cb237775e9e44f605b0eb5c08588f1ffb59
Sean Li <lishang106@gmail.com>
2014-05-14 20:12:54 -0400

    Using post now.

--- web/js/backend.js
+++ web/js/backend.js
@@ -5,24 +5,8 @@ var API = function () {
 
   return function (resource, data, callback) {
     var url  = API_URL + resource;
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: JSON.stringify(data || {}),
-      dataType: "jsonp",
-      timeout: TIMEOUT,
-      crossDomain: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      success: function (data) {
-        console.log(data);
-      },
-      error: function (request, status, err) {
-        console.log(request);
-        console.log(status);
-        console.log(err);
-      }
+    $.post(url, data, function (data) {
+      console.log(data);
     });
   };
 
